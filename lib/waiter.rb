e0b6c8c566d0522b1524269011dbaa10b5106c1d
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-10 19:40:06 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -29,7 +29,7 @@ class Waiter
     result = []
     best_tipped_meal = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip
-      binding.pry
+      #binding.pry
     end
     best_tipped_meal
     binding.pry
